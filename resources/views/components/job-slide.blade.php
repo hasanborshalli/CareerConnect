57587e0fb2099cd0489ec5197417ea220ad274cd
Hasan Borshalli <hasanborshalli@gmail.com>
2024-11-24 15:26:19 +0200

    editing job-slide

--- resources/views/components/job-slide.blade.php
+++ resources/views/components/job-slide.blade.php
@@ -1,4 +1,4 @@
-
+{{-- 
 <div class="job-slide">
     <div class="job-company">
         <div class="company-photo">
@@ -26,5 +26,32 @@
         <button class="job-slide-btn" onclick="window.location.href = '/jobs/{{$jobId}}'">Details</button>
 
     </div>
+</div> --}}
+<div class="job-slide">
+<div class="job-container">
+    <div class="job-main">
+        <img src="/storage/companies_avatars/{{$companyAvatar}}" alt="{!! $companyName !!}">
+        <h2 class="job-title">{!! $jobTitle !!}</h2>
+    </div>
+    <div class="job-info">
+        <h4>{!! $companyName !!}</h4>
+        <p>{!! $jobCategory !!}</p>
+        <p>📍 {!! $jobLocation !!}</p>
+    </div>
 </div>
+ <div class="job-desc"><p>{!! $jobDescription !!}...</p></div>
+   <div class="job-btn">
+        @auth
+        @if(auth()->user()->role=='seeker')
+        <button class="job-slide-btn" onclick="window.location.href = '/jobs/{{$jobId}}?apply=yes'">Apply Now</button>
+        @elseif(auth()->user()->role=='company')
+        <button class="job-slide-btn" onclick="window.location.href = '/jobs/{{$jobId}}'">Apply Now</button>
+        @endif
+        @endauth 
+        @guest
+        <button class="job-slide-btn" onclick="window.location.href = '/jobs/{{$jobId}}'">Apply Now</button>
+        @endguest
+        <button class="job-slide-btn" onclick="window.location.href = '/jobs/{{$jobId}}'">Details</button>
 
+    </div>
+</div>
